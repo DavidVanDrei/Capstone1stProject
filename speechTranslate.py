27efa20ea39f9ab84e253c4db15b48fce47d35e7
naishilpatel <naishil.patel@gmail.com>
2020-08-27 16:26:47 -0400

    Added language selection options

--- speechTranslate.py
+++ speechTranslate.py
@@ -2,8 +2,8 @@ from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 import json
 
-url = ''
-apikey = ''
+url = '' # enter your URL
+apikey = '' # enter your API key
 
 authenticator = IAMAuthenticator(apikey)
 language_translator = LanguageTranslatorV3(
@@ -13,14 +13,26 @@ language_translator = LanguageTranslatorV3(
 
 language_translator.set_service_url(url)
 
-print("Enter a Sentence in English to be translated")
+langs = {
+    "english": "en",
+    "french": "fr",
+    "german": "de",
+    "spanish": "es",
+    "hindi": "hi"
+}
+
+print("Choose a language to translate from (english, french, german, spanish, hindi): ")
+source = input()
+print("Choose a language to translate into (english, french, german, spanish, hindi): ")
+target = input()
+print("Enter a Sentence to be translated")
 text = input()
 
 from ibm_watson import ApiException
 try:
     translation = language_translator.translate(
-    text='Hello, how are you today?',
-    model_id='en-es').get_result()
+    text=text,
+    source=source, target=target).get_result()
     print(json.dumps(translation, indent=2, ensure_ascii=False))
 except ApiException as ex:
     print("Method failed with status code " + str(ex.code) + ": " + ex.message)
